5bd320d6ccc893719dbe448fdc53c3ef253cd5a8
yakov152005 <yakov152005@gmail.com>
2024-12-08 21:09:34 +0200

    git commit -m "הודעה על השינויים שבוצעו בפרויקט"

--- Main.sql
+++ Main.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE space_sql;
+/*CREATE DATABASE space_sql;*/
 -- 1. שלוף את הדאטות של כל החייזרים בעלי שתי רגליים
 SELECT *
 FROM aliens
@@ -25,7 +25,7 @@ FROM trips
          JOIN ships ON trips.s_id = ships.s_id;
  */
 
--- 5. שלוף את שמות החייזרים שהשתתפו במסע לפני שנת 2000
+-- 4. שלוף את שמות החייזרים שהשתתפו במסע לפני שנת 2000
 SELECT DISTINCT alien_name
 FROM aliens, aliens_in_trips, trips
 WHERE trips.t_id = aliens_in_trips.t_id
@@ -39,17 +39,17 @@ JOIN trips ON aliens_in_trips.t_id = trips.t_id
 WHERE trips.date_time < '2000-01-01';
  */
 
--- 6. מצא את המהירות המירבית של כלל חלליות הצי הגלקסי
+-- 5. מצא את המהירות המירבית של כלל חלליות הצי הגלקסי
 SELECT MAX(max_speed) AS higher_max_speed
 FROM ships;
 
--- 7. מצא את האוכלוסיה הממוצעת בכוכבי הלכת של כל מערכת השמש
+-- 6. מצא את האוכלוסיה הממוצעת בכוכבי הלכת של כל מערכת השמש
 SELECT constellation, AVG(population) AS avg_population
 FROM planets
 GROUP BY constellation;
 
 
--- 8. מצא עבור כל חייזר כמה פעמים ביקר בכל כוכב לכת
+-- 7. מצא עבור כל חייזר כמה פעמים ביקר בכל כוכב לכת
 SELECT
     aliens.alien_name,
     planets.plant_name,
@@ -79,6 +79,41 @@ GROUP BY
     aliens.alien_name, planets.plant_name;
 
  */
+
+-- 8. מצא את שמות כל החייזרים שהשתתפו ביותר משני מסעות
+SELECT
+   aliens.alien_name,
+       COUNT(*) AS name_of_alien_up_2_trips
+FROM
+    aliens
+JOIN
+    aliens_in_trips ON aliens.id_num = aliens_in_trips.id_num
+JOIN
+    trips ON trips.t_id = aliens_in_trips.t_id
+GROUP BY
+    aliens.alien_name
+HAVING
+    COUNT(*) > 2;
+
+-- 9. מצא עבור כל כוכב לכת את מספר הרגליים שדרכו עליו במידה והיו יותר מ - 15
+SELECT
+    planets.plant_name,
+    SUM(aliens.num_of_legs) AS total_legs
+FROM
+    aliens
+JOIN
+    aliens_in_trips ON aliens.id_num = aliens_in_trips.id_num
+JOIN
+    trips ON trips.t_id = aliens_in_trips.t_id
+JOIN
+    planets ON planets.p_id = trips.p_id
+GROUP BY
+    planets.plant_name
+HAVING
+    SUM(aliens.num_of_legs) > 15;
+
+
+
 
 
 
